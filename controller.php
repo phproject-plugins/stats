2e6edea66030e19afc10d4e1da34f75904a29454
Alan Hardman <ahardman@thrivelife.com>
2015-12-01 11:14:54 -0700

    Adding some basic issue stats

--- controller.php
+++ controller.php
@@ -42,8 +42,43 @@ class Controller extends \Controller {
 				LIMIT 25");
 		$f3->set("user_hours", $result);
 
+		// Issue stats
+		$result = $db->exec("SELECT SUM(hours_spent) num FROM issue");
+		$f3->set("issues_total_hours_spent", $result[0]['num']);
 
-		// TODO: add issue stats
+		$result = $db->exec("SELECT AVG(hours_spent) num
+				FROM issue
+				WHERE hours_spent > 0
+					AND hours_spent IS NOT NULL");
+		$f3->set("issues_avg_hours_spent", $result[0]['num']);
+
+		$result = $db->exec("SELECT COUNT(*) num FROM issue WHERE closed_date IS NOT NULL");
+		$f3->set("issues_total_closed", $result[0]['num']);
+
+		// Top issue stats
+		$result = $db->exec("SELECT i.id, i.name, COUNT(*) num
+				FROM issue i
+				JOIN issue_comment c ON c.issue_id = i.id
+				GROUP BY i.id
+				ORDER BY num DESC
+				LIMIT 25");
+		$f3->set("top_issue_comments", $result);
+
+		$result = $db->exec("SELECT i.id, i.name, COUNT(*) num
+				FROM issue i
+				JOIN issue_update u ON u.issue_id = i.id
+				GROUP BY i.id
+				ORDER BY num DESC
+				LIMIT 25");
+		$f3->set("top_issue_updates", $result);
+
+		$result = $db->exec("SELECT i.id, i.name, i.hours_spent num
+				FROM issue i
+				WHERE hours_spent > 0
+					AND hours_spent IS NOT NULL
+				ORDER BY hours_spent DESC
+				LIMIT 25");
+		$f3->set("top_issue_hours", $result);
 
 
 		// Render view
